d23ee82aaf125f885b72a0922b3704708facee02
chanukyababu <chanukyababu0414@gmail.com>
2024-02-08 22:45:04 +0530

    modification2

--- chatproject/chatapp/models.py
+++ chatproject/chatapp/models.py
@@ -15,7 +15,7 @@ class Message(models.Model):
 
     def __str__(self):
         return f'Message from {self.user.username} at {self.created_at}'"""
-from django.db import models
+"""from django.db import models
 
 class Message(models.Model):
     # Define your fields here
@@ -26,3 +26,15 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.sender}: {self.content}"
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'chatapp_message' 
